e551320e6c28eb87b4a847346dd0f5d160b8a5e6
SoftSerbia <tech@softserbia.com>
2015-01-29 12:51:04 +0100

    Implement defaults overrides through settings

--- image_hiduken.js
+++ image_hiduken.js
@@ -8,7 +8,7 @@
  *    copyright 2012 John Koht
  *    Released under the MIT and GPL Licenses.
  *
- *    ----------------------------------------------------- 
+ *    -----------------------------------------------------
  *
  *    author:   John Koht
  *    version:  0.1.0
@@ -17,7 +17,8 @@
  */
 
 (function($) {
-  
+
+  var settings;
   /*
   ---------------------------------------------------------------
     Image Hiduken Plugin. This just instantiates the plugin
@@ -25,12 +26,12 @@
   */
   $.fn.image_hiduken = function(options) {
     // Setup the settings
-    var settings = $.extend({}, $.fn.image_hiduken.defaults, options);
+    settings = $.extend({}, $.fn.image_hiduken.defaults, options);
     $.fn.image_hiduken.vars.elements = this;
     // Initiate the plugin
     image_hiduken.init();
   };
-  
+
   // Defaults, these are configurable by the user
   $.fn.image_hiduken.defaults = {
     default_to_small : true,
@@ -38,7 +39,7 @@
     medium_size : 640,
     large_size : 980,
   };
-  
+
   // Standard variables
   $.fn.image_hiduken.vars = {
     elements : null,
@@ -46,48 +47,45 @@
     screen_height : 0,
     current_size : $.fn.image_hiduken.defaults.default_to_small ? $.fn.image_hiduken.defaults.small_size : 0,
   }
-  
-  
-  
-  
+
   /*
   ---------------------------------------------------------------
     image_hiduken object. Most of the methods for resize and
     image updates
   */
   var image_hiduken = {
-    
-    // init functions  
+
+    // init functions
     init: function() {
       image_hiduken.bind_to_resize();
       image_hiduken.resize_handler();
     },
-    
-    
+
+
     // Bind to window resize event to trigger our resize handler
     bind_to_resize: function() {
       $(window).resize(function() {
-        $.fn.image_hiduken.vars.screen_width = $(window).width();
+        $.fn.image_hiduken.vars.screen_width = window.innerWidth || $(window).width();
         $.fn.image_hiduken.vars.screen_height = $(window).height();
         image_hiduken.resize_handler();
       });
     },
-    
-    
-    resize_handler: function() {      
+
+
+    resize_handler: function() {
       // Let's make sure the screen width and height are NOT set to 0. If so, let's set them
       // to the screen resolution
       if ($.fn.image_hiduken.vars.screen_width == 0 && $.fn.image_hiduken.vars.screen_height == 0) {
-        $.fn.image_hiduken.vars.screen_width = $(window).width();
-        $.fn.image_hiduken.vars.screen_height = $(window).height();        
-      }      
+        $.fn.image_hiduken.vars.screen_width = window.innerWidth || $(window).width();
+        $.fn.image_hiduken.vars.screen_height = $(window).height();
+      }
       // Set up our array of sizes and the closest match as null
       var size = null;
       var size_key = null;
       var sizes = {
-        small_size : $.fn.image_hiduken.defaults.small_size,
-        medium_size : $.fn.image_hiduken.defaults.medium_size,
-        large_size : $.fn.image_hiduken.defaults.large_size,
+        small_size : settings.small_size,
+        medium_size : settings.medium_size,
+        large_size : settings.large_size,
       };
       // Loop over our size array to figure out which size is the closest to our screen size
       $.each(sizes, function(key, value) {
@@ -96,18 +94,18 @@
           size_key = key;
         }
       });
-      image_hiduken.update_images(size_key, size);      
+      image_hiduken.update_images(size_key, size);
     },
-    
-    
+
+
     // Update the actual images
     update_images: function(key, size) {
       // Set up our data attribute
-      var data_attribute = "data-" + key.replace("_", "-").replace('size', 'src');      
+      var data_attribute = "data-" + key.replace("_", "-").replace('size', 'src');
       // Iterate over all the images and make the appropriate updates
-      $.fn.image_hiduken.vars.elements.each(function(index, item) {        
+      $.fn.image_hiduken.vars.elements.each(function(index, item) {
         // Let's make sure the data attribute exists
-        if ( $(item).attr(data_attribute) != null ) {        
+        if ( $(item).attr(data_attribute) != null ) {
           // If the item is a div, let's set the background image
           if ( $(item).is('div') ) {
             $(item).css({
@@ -123,8 +121,6 @@
       });
     },
 
-    
   };
-  
-  
+
 })(jQuery);
